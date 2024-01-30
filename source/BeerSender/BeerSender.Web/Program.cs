72d9c312e0efe8193f5ff5682bc4afdf3ad5d258
Hannes Lowette <lowette.hannes@gmail.com>
2024-01-30 12:16:23 +0100

    Registered DbContext

--- source/BeerSender/BeerSender.Web/Program.cs
+++ source/BeerSender/BeerSender.Web/Program.cs
@@ -1,5 +1,7 @@
+using BeerSender.Web.Event_stream;
 using BeerSender.Web.Hubs;
 using BeerSender.Web.JsonHelpers;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,11 @@ builder.Services.AddControllers()
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Event_context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Event_context"));
+});
 
 var app = builder.Build();
 
